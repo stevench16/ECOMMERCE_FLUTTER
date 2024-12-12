5ed2783bddf4719fe31ad0fdc36fdd455b8b128a
Luis-lhlm2024 <luisleguizamo@hotmail.es>
2024-12-12 08:53:43 -0500

    Cambios en lib/main.dart, Luis Leguizamo.

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+    ||initialRoute: 'login',
       routes: {
          'login': (BuildContext context) => LoginPage()
         },
