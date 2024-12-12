ef541f77e5050673cfb916e59318539775f2a432
Luis-lhlm2024 <luisleguizamo@hotmail.es>
2024-12-12 12:06:03 -0500

    Se modifica el constructor 'const' en LoginPage

--- lib/src/presentation/pages/auth/login/LoginPage.dart
+++ lib/src/presentation/pages/auth/login/LoginPage.dart
@@ -5,7 +5,7 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
           child: Text('LoginPage'),
         )
