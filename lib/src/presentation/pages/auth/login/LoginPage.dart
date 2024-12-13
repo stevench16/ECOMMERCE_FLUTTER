5d79265c5579e046632c8bac1707fbafb1a45b8f
Luis-lhlm2024 <luisleguizamo@hotmail.es>
2024-12-13 09:20:01 -0500

    Se agrega campos Textfield en LoginPage

--- lib/src/presentation/pages/auth/login/LoginPage.dart
+++ lib/src/presentation/pages/auth/login/LoginPage.dart
@@ -6,10 +6,36 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Text('LoginPage'),
+        body: Container(
+          width: double.infinity,
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.person,
+                color: Colors.black,
+                size: 125,
+              ),
+              Text(
+                'LOGIN',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Correo Electrónico')
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Contraseña')
+                ),
+            ],
+          ),
         )
-
     );
   }
 }
