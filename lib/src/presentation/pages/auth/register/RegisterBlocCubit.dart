93880eedca0c38f29eef089bbbdd73d163143d7f
Luis-lhlm2024 <luisleguizamo@hotmail.es>
2025-01-10 17:03:17 -0500

    Manejo de estados campos formulario de registro

--- lib/src/presentation/pages/auth/register/RegisterBlocCubit.dart
+++ lib/src/presentation/pages/auth/register/RegisterBlocCubit.dart
@@ -15,6 +15,100 @@ final _confirmpasswordController = BehaviorSubject<String>();
 
 Stream<String> get nameStream => _nameController.stream;
 Stream<String> get lastnameStream => _lastnameController.stream;
+Stream<String> get emailStream => _emailController.stream;
+Stream<String> get phoneStream => _phoneController.stream;
+Stream<String> get passwordStream => _passwordController.stream;
+Stream<String> get confirmpasswordStream => _confirmpasswordController.stream;
 
+Stream<bool> get validateForm => Rx.combineLatest6(
+  nameStream,
+  lastnameStream, 
+  emailStream, 
+  phoneStream, 
+  passwordStream, 
+  confirmpasswordStream, (a, b, c, d, e, f) => true
+  );
+
+  void register(){
+    print('Name: ${_nameController.value}');
+    print('Lastname: ${_lastnameController.value}');
+    print('Email: ${_emailController.value}');
+    print('Phone: ${_phoneController.value}');
+    print('Password: ${_passwordController.value}');
+    print('Confirm Password: ${_confirmpasswordController.value}');
+
+  }
+
+void changeName(String name){
+  if (name.length < 2){
+    _nameController.sink.addError('El nombre es muy corto');
+  }
+  else {
+    _nameController.sink.add(name);
+  }
+}
+
+void changeLastName(String lastname){
+  if (lastname.length < 2){
+    _lastnameController.sink.addError('El apellido es muy corto');
+  }
+  else {
+    _lastnameController.sink.add(lastname);
+  }
+}
+
+void changeEmail(String email) {
+    bool emailFormatValid = 
+    RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(email);
+    if (email.isNotEmpty && email.length < 5) {
+      _emailController.sink.addError('Al menos 5 caracteres');
+    }
+    else if(email.isNotEmpty && !emailFormatValid) {
+      _emailController.sink.addError('El email no es valido');
+    }
+    else {
+      _emailController.sink.add(email);
+    }
+  }
+
+void changePhone(String phone){
+  if (phone.length < 8){
+    _phoneController.sink.addError('No es un numero de telefono valido');
+  }
+  else {
+    _phoneController.sink.add(phone);
+  }
+}
+
+void changePassword(String password){
+  if (password.length < 6){
+    _passwordController.sink.addError('Al menos 6 caracteres');
+  }
+  else {
+    _passwordController.sink.add(password);
+  }
+}
+
+void changeConfirmPassword(String confirmPassword){
+  if (confirmPassword.length < 6){
+    _confirmpasswordController.sink.addError('Al menos 6 caracteres');
+  }
+  else if (confirmPassword != _passwordController.value){
+    _confirmpasswordController.sink.addError('Las contraseñas no coinciden');
+  }
+  else {
+    _confirmpasswordController.sink.add(confirmPassword);
+  }
+}
+
+void dispose(){
+  changeName('');
+  changeLastName('');
+  changeEmail('');
+  changePhone('');
+  changePassword('');
+  changeConfirmPassword('');
+}
 
 }
--- lib/src/presentation/pages/auth/register/RegisterPage.dart
+++ lib/src/presentation/pages/auth/register/RegisterPage.dart
@@ -1,13 +1,34 @@
+import 'package:ecommerce_flutter/src/presentation/pages/auth/register/RegisterBlocCubit.dart';
 import 'package:ecommerce_flutter/src/presentation/pages/auth/widgets/DefaultButton.dart';
 import 'package:ecommerce_flutter/src/presentation/pages/auth/widgets/DefaultIconBack.dart';
 import 'package:ecommerce_flutter/src/presentation/pages/auth/widgets/DefaultTextfield.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+
+  RegisterBlocCubit? _registerBlocCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _registerBlocCubit?.dispose();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    
+    _registerBlocCubit = BlocProvider.of<RegisterBlocCubit>(context, listen: false);
+
     return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
