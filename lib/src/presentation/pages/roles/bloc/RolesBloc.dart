60d7d01bb2d34f78ba8d59691760a73fb76acf19
stevench16 <steven_chavez16@hotmail.com>
2025-02-10 10:59:03 -0500

    (App) Creación de la pantalla de roles.

---
+++ lib/src/presentation/pages/roles/bloc/RolesBloc.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce_flutter/src/domain/models/AuthResponse.dart';
+import 'package:ecommerce_flutter/src/domain/repository/AuthRepository.dart';
+import 'package:ecommerce_flutter/src/domain/useCases/auth/AuthUseCases.dart';
+import 'package:ecommerce_flutter/src/presentation/pages/roles/bloc/RolesEvent.dart';
+import 'package:ecommerce_flutter/src/presentation/pages/roles/bloc/RolesState.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RolesBloc extends Bloc<RolesEvent, RolesState> {
+  AuthUseCases authUseCases;
+
+  RolesBloc(this.authUseCases) : super(RolesState()) {
+    on<GetRolesList>(_onGetRolesList);
+  }
+
+  Future<void> _onGetRolesList(
+      GetRolesList event, Emitter<RolesState> emit) async {
+    AuthResponse? authResponse = await authUseCases.getUSerSession.run();
+    emit(state.copywith(roles: authResponse?.user.roles));
+  }
+}
---
+++ lib/src/presentation/pages/roles/bloc/RolesEvent.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+abstract class RolesEvent extends Equatable {
+  const RolesEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class GetRolesList extends RolesEvent {
+  const GetRolesList();
+}
